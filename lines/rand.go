cb59681d374fd68486835b7b90146288f6f109bb
Michal Vala <michal.vala@gmail.com>
2019-02-11 06:01:43 +0100

    add random line each frame

---
+++ lines/rand.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/sparkoo/sparkengine/core"
+	"github.com/sparkoo/sparkengine/scene"
+	"github.com/sparkoo/sparkengine/scene/olib/line"
+	"math/rand"
+	"time"
+)
+
+//noinspection ALL
+const (
+	FPS    = 30
+	WIDTH  = 640
+	HEIGHT = 480
+)
+
+//noinspection ALL
+var (
+	g *core.Game
+	c *core.Conf
+	s *scene.Scene
+)
+
+func init() {
+	c = core.NewConf(FPS, FPS*2, WIDTH, HEIGHT)
+	g = core.NewGame(c)
+	s = scene.NewScene(addRandomLineTick)
+
+	rand.Seed(time.Now().UnixNano())
+}
+
+func addRandomLineTick() {
+	s.AddObject(line.NewLine(
+		rand.Float64()*WIDTH,
+		rand.Float64()*HEIGHT,
+		rand.Float64()*WIDTH,
+		rand.Float64()*HEIGHT,
+		scene.RandomColor(50)))
+}
+
+func main() {
+	g.Start(s)
+}
