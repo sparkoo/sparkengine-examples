24d3b32901cef75d9e2221ba77d07699de83057d
Michal Vala <michal.vala@gmail.com>
2019-02-12 10:42:50 +0100

    sprite example

---
+++ custom_sprite/sprite.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/sparkoo/sparkengine/core"
+	"github.com/sparkoo/sparkengine/scene"
+	"github.com/sparkoo/sparkengine/scene/olib/sprite"
+)
+
+func main() {
+	g := core.NewGame(core.NewConf(30, 60, 640, 480))
+	s := scene.NewScene(scene.NoopTick)
+
+	s.AddObject(sprite.NewSprite(100, 100, 15, 10, initPixels))
+
+	g.Start(s)
+}
