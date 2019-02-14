550c1edac6ad2dd7ed62d883e64279a8ddeb7f40
Michal Vala <michal.vala@gmail.com>
2019-02-14 15:44:05 +0100

    background asset

--- control_animation/main.go
+++ control_animation/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"github.com/sparkoo/sparkengine/core"
 	"github.com/sparkoo/sparkengine/core/scene"
+	"github.com/sparkoo/sparkengine/core/scene/image"
+	"github.com/sparkoo/sparkengine/core/scene/olib/sprite"
 )
 
 func main() {
 	g := core.NewGame(core.NewConf30T960W())
 
 	character := NewCharacter(0, 0)
+	background := sprite.NewSprite(0, 0, 960, 540, func() []scene.Pixel {
+		return image.LoadImage("assets/background.png", 0, 0, 960, 540)
+	})
 
 	s := scene.NewScene(func(gameTickCounter int64, sceneTickCounter int64) {
 		if gameTickCounter%3 == 0 {
@@ -16,7 +21,7 @@ func main() {
 		}
 	})
 
-	s.AddObject(character)
+	s.AddObjects(background, character)
 
 	g.Start(s)
 }
