d60a4b0117ec606603f62e0cf88a52a3c48e5f5d
Michal Vala <michal.vala@gmail.com>
2019-03-02 15:15:29 +0100

    bouncing balls

---
+++ bouncing-balls/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"github.com/sparkoo/sparkengine/core"
+	"github.com/sparkoo/sparkengine/core/scene"
+	"github.com/sparkoo/sparkengine/core/scene/olib/shape"
+	"math/rand"
+)
+
+const WIDTH = 640
+const HEIGHT = 240
+
+func main() {
+	g := core.NewGame(core.NewConf30T640W())
+
+	balls := generateRandomBalls(100)
+
+	s := scene.NewScene(func(gameTickCounter int64, sceneTickCounter int64) {
+		for _, b := range balls {
+			x, y := b.GetPos()
+
+			xpot := x + b.vx
+			if !(xpot > 0 && (int(xpot)+b.GetXsize()) < WIDTH) {
+				b.vx = b.vx * -1
+			}
+
+			ypot := y + b.vy
+			if !(ypot > 0 && (int(ypot)+b.GetYsize()) < HEIGHT) {
+				b.vy = b.vy * -1
+			}
+			b.MoveBy(b.vx, b.vy)
+		}
+	})
+
+	for _, b := range balls {
+		s.AddObject(b)
+	}
+
+	g.Start(s)
+}
+
+func generateRandomBalls(n int) []movingBall {
+	balls := make([]movingBall, n)
+	for i := 0; i < n; i++ {
+		balls[i] = movingBall{
+			Ball: shape.NewBall(320, 240, rand.Intn(30), scene.RandomColor(255)),
+			vx:   (rand.Float64() - .5) * 5,
+			vy:   (rand.Float64() - .5) * 5}
+	}
+	return balls
+}
+
+type movingBall struct {
+	*shape.Ball
+	vx float64
+	vy float64
+}
